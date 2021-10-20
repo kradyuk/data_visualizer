709fd37df6f0605a074c8ea632ae8eeee6d18b86
admin <admin@192.168.1.5>
2021-10-20 21:40:17 +0300

    Initialize original branch with original data

--- app.py
+++ app.py
@@ -2,76 +2,127 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
-st.title("Анализ комментариев инстаграм-аккаунта @uo_ggkttid")
-st.sidebar.title("Боковая панель настройки")
 
-st.markdown("Ресурс представляет собой панели визуализации для анализа комментариев на примере инстаграм-аккаунта @uo_ggkttid 📊")
-st.sidebar.markdown("Анализ комментариев инстаграм-аккаунта @uo_ggkttid")
+DATA_URL = (
+#    "/home/cicada/Downloads/rhyme/streamlit-sentiment/Tweets.csv"
+    "Tweets.csv"
+)
 
-#DATA_URL = ("/Users/admin/Projects/py_example/data.csv")
-DATA_URL = ("data.csv")
+st.title("Sentiment Analysis of Tweets about US Airlines")
+st.sidebar.title("Sentiment Analysis of Tweets")
+st.markdown("This application is a Streamlit dashboard used "
+            "to analyze sentiments of tweets 🐦")
+st.sidebar.markdown("This application is a Streamlit dashboard used "
+            "to analyze sentiments of tweets 🐦")
 
-global fig
-
-@st.cache(persist=False)
+@st.cache(persist=True)
 def load_data():
-    data = pd.read_csv(DATA_URL, encoding='utf-8')
+    data = pd.read_csv(DATA_URL)
     data['tweet_created'] = pd.to_datetime(data['tweet_created'])
     return data
 
 data = load_data()
 
-st.sidebar.subheader("Показать случайный комментарий")
+st.sidebar.subheader("Show random tweet")
 random_tweet = st.sidebar.radio('Sentiment', ('positive', 'neutral', 'negative'))
-st.sidebar.markdown(data.query('airline_sentiment == @random_tweet')[["text"]].sample(n=1).iat[0,0])
+st.sidebar.markdown(data.query("airline_sentiment == @random_tweet")[["text"]].sample(n=1).iat[0, 0])
 
-st.sidebar.markdown("### Количественное сравнение комментариев")
-select = st.sidebar.selectbox('Тип визуализации', ['Гистограмма', 'Круговая диаграмма'], key='1')
+st.sidebar.markdown("### Number of tweets by sentiment")
+select = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='1')
 sentiment_count = data['airline_sentiment'].value_counts()
 sentiment_count = pd.DataFrame({'Sentiment':sentiment_count.index, 'Tweets':sentiment_count.values})
-
-if not st.sidebar.checkbox("Скрыть", True):
-    st.markdown("### Количественное сравнение комментариев")
-    if select == "Гистограмма":
-        fig = px.bar(sentiment_count,x='Sentiment',y='Tweets',color='Tweets', height=500)
+if not st.sidebar.checkbox("Hide", True):
+    st.markdown("### Number of tweets by sentiment")
+    if select == 'Bar plot':
+        fig = px.bar(sentiment_count, x='Sentiment', y='Tweets', color='Tweets', height=500)
         st.plotly_chart(fig)
     else:
         fig = px.pie(sentiment_count, values='Tweets', names='Sentiment')
         st.plotly_chart(fig)
 
-
-st.sidebar.subheader("Когда и откуда пользователи оставляли комментарии?")
-hour = st.sidebar.number_input("Время суток",min_value=1, max_value=24)
+st.sidebar.subheader("When and where are users tweeting from?")
+hour = st.sidebar.slider("Hour to look at", 0, 23)
 modified_data = data[data['tweet_created'].dt.hour == hour]
-if not st.sidebar.checkbox("Скрыть", True, key='1'):
-    st.markdown("### Местоположение пользователей оставивших комментарии в зависимости от времени суток")
-    st.markdown("%i комментариев в период с %i:00 до %i:00" % (len(modified_data), hour, (hour+1)%24))
+if not st.sidebar.checkbox("Close", True, key='1'):
+    st.markdown("### Tweet locations based on time of day")
+    st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour + 1) % 24))
     st.map(modified_data)
-    if st.sidebar.checkbox("Показать необработанные данные", False):
+    if st.sidebar.checkbox("Show raw data", False):
         st.write(modified_data)
 
-st.sidebar.subheader("Распределение комментариев по годам")
-choice = st.sidebar.multiselect('Выбрать год', ('2020', '2021', '2019'), key='0')
 
+st.sidebar.subheader("Total number of tweets for each airline")
+each_airline = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='2')
+airline_sentiment_count = data.groupby('airline')['airline_sentiment'].count().sort_values(ascending=False)
+airline_sentiment_count = pd.DataFrame({'Airline':airline_sentiment_count.index, 'Tweets':airline_sentiment_count.values.flatten()})
+if not st.sidebar.checkbox("Close", True, key='2'):
+    if each_airline == 'Bar plot':
+        st.subheader("Total number of tweets for each airline")
+        fig_1 = px.bar(airline_sentiment_count, x='Airline', y='Tweets', color='Tweets', height=500)
+        st.plotly_chart(fig_1)
+    if each_airline == 'Pie chart':
+        st.subheader("Total number of tweets for each airline")
+        fig_2 = px.pie(airline_sentiment_count, values='Tweets', names='Airline')
+        st.plotly_chart(fig_2)
+
+
+@st.cache(persist=True)
+def plot_sentiment(airline):
+    df = data[data['airline']==airline]
+    count = df['airline_sentiment'].value_counts()
+    count = pd.DataFrame({'Sentiment':count.index, 'Tweets':count.values.flatten()})
+    return count
+
+
+st.sidebar.subheader("Breakdown airline by sentiment")
+choice = st.sidebar.multiselect('Pick airlines', ('US Airways','United','American','Southwest','Delta','Virgin America'))
+if len(choice) > 0:
+    st.subheader("Breakdown airline by sentiment")
+    breakdown_type = st.sidebar.selectbox('Visualization type', ['Pie chart', 'Bar plot', ], key='3')
+    fig_3 = make_subplots(rows=1, cols=len(choice), subplot_titles=choice)
+    if breakdown_type == 'Bar plot':
+        for i in range(1):
+            for j in range(len(choice)):
+                fig_3.add_trace(
+                    go.Bar(x=plot_sentiment(choice[j]).Sentiment, y=plot_sentiment(choice[j]).Tweets, showlegend=False),
+                    row=i+1, col=j+1
+                )
+        fig_3.update_layout(height=600, width=800)
+        st.plotly_chart(fig_3)
+    else:
+        fig_3 = make_subplots(rows=1, cols=len(choice), specs=[[{'type':'domain'}]*len(choice)], subplot_titles=choice)
+        for i in range(1):
+            for j in range(len(choice)):
+                fig_3.add_trace(
+                    go.Pie(labels=plot_sentiment(choice[j]).Sentiment, values=plot_sentiment(choice[j]).Tweets, showlegend=True),
+                    i+1, j+1
+                )
+        fig_3.update_layout(height=600, width=800)
+        st.plotly_chart(fig_3)
+st.sidebar.subheader("Breakdown airline by sentiment")
+choice = st.sidebar.multiselect('Pick airlines', ('US Airways','United','American','Southwest','Delta','Virgin America'), key=0)
 if len(choice) > 0:
     choice_data = data[data.airline.isin(choice)]
-    fig_choice = px.histogram(choice_data, x='airline', y='airline_sentiment', histfunc='count', color='airline_sentiment',
-    facet_col='airline_sentiment',labels={'airline_sentiment':'tweets'}, height=600, width=800)
-    st.plotly_chart(fig_choice)
+    fig_0 = px.histogram(
+                        choice_data, x='airline', y='airline_sentiment',
+                         histfunc='count', color='airline_sentiment',
+                         facet_col='airline_sentiment', labels={'airline_sentiment':'tweets'},
+                          height=600, width=800)
+    st.plotly_chart(fig_0)
 
-st.sidebar.header("Облако комментариев")
-word_sentiment = st.sidebar.radio('sentiment', ('positive','neutral', 'negative'))
-st.set_option('deprecation.showPyplotGlobalUse', False)
-
-if not st.sidebar.checkbox("Скрыть", True, key='3'):
-    st.header('Word cloud for %s sentiment' % (word_sentiment))
+st.sidebar.header("Word Cloud")
+word_sentiment = st.sidebar.radio('Display word cloud for what sentiment?', ('positive', 'neutral', 'negative'))
+if not st.sidebar.checkbox("Close", True, key='3'):
+    st.subheader('Word cloud for %s sentiment' % (word_sentiment))
     df = data[data['airline_sentiment']==word_sentiment]
     words = ' '.join(df['text'])
     processed_words = ' '.join([word for word in words.split() if 'http' not in word and not word.startswith('@') and word != 'RT'])
-    wordcloud = WordCloud(stopwords=STOPWORDS, background_color='white', height=640, width=800).generate(processed_words)
+    wordcloud = WordCloud(stopwords=STOPWORDS, background_color='white', width=800, height=640).generate(processed_words)
     plt.imshow(wordcloud)
     plt.xticks([])
     plt.yticks([])
